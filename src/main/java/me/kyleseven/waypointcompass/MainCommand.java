5f69c41b12d7435d1e40cc8fee746c91d969d21b
kyleseven <kyle.senerpida@icloud.com>
2020-04-09 17:55:16 -0500

    Formatting on help subcommand

--- src/main/java/me/kyleseven/waypointcompass/MainCommand.java
+++ src/main/java/me/kyleseven/waypointcompass/MainCommand.java
@@ -19,12 +19,12 @@ public class MainCommand extends BaseCommand {
     @Default
     public void doHelp(CommandSender sender) {
         String[] help = {"&7---- &cWaypointCompass Help &7----",
-                "&c/wc &7help &7- Displays the plugin commands.",
-                "&c/wc &7current &7- Displays the current compass heading.",
-                "&c/wc &7set &c<x> <y> <z> &7- Sets the compass to the specified coordinate.",
-                "&c/wc &7reset &7- Resets the compass back to spawn.",
-                "&c/wc &7reload &7- Reloads the plugin configuration.",
-                "&c/wc &7version &7- See the version of the plugin."};
+                         "&c/wc &7help &7- Displays the plugin commands.",
+                         "&c/wc &7current &7- Displays the current compass heading.",
+                         "&c/wc &7set &c<x> <y> <z> &7- Sets the compass to the specified coordinate.",
+                         "&c/wc &7reset &7- Resets the compass back to spawn.",
+                         "&c/wc &7reload &7- Reloads the plugin configuration.",
+                         "&c/wc &7version &7- See the version of the plugin."};
         for (String s : help) {
             Utils.sendMsg(sender, s);
         }
