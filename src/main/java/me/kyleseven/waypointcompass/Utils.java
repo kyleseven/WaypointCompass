9c3dbd38036bd5063d26f79fd9be783a434efb35
kyleseven <kyle.senerpida@icloud.com>
2020-04-09 20:16:00 -0500

    New Util: sendPrefixMsg()

--- src/main/java/me/kyleseven/waypointcompass/Utils.java
+++ src/main/java/me/kyleseven/waypointcompass/Utils.java
@@ -1,10 +1,15 @@
 package me.kyleseven.waypointcompass;
 
+import me.kyleseven.waypointcompass.config.MsgConfig;
 import org.bukkit.ChatColor;
 import org.bukkit.command.CommandSender;
 
 public final class Utils {
     public static void sendMsg(CommandSender sender, String msg) {
         sender.sendMessage(ChatColor.translateAlternateColorCodes('&', msg));
+    }
+
+    public static void sendPrefixMsg(CommandSender sender, String msg) {
+        sender.sendMessage(ChatColor.translateAlternateColorCodes('&', MsgConfig.getInstance().getPrefix() + " " + msg));
     }
 }
